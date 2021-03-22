97e497d5bfe674c9fd615b8df356b2133fe35b9f
Eugene Musebe <eugenemusebe@Eugenes-MacBook-Pro.local>
2021-03-22 13:30:38 +0300

    solved env issue

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      images: {
-        domains: ['res.cloudinary.com'],
-      },
-    };
-  }
-
+module.exports = () => {
   return {
     images: {
       domains: ['res.cloudinary.com'],
